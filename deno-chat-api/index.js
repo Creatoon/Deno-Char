b62f6725258eed1870f49c90d03498a9a9318a4b
Creatoon <yasharyan1099@gmail.com>
2022-12-20 21:02:25 +0530

    update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (e) => {
-  const res = new Response("Hello World!", {
+  const res = new Response("Hello World! 2nd try", {
     headers: { "content-type": "text/plain" },
   });
   e.respondWith(res);
